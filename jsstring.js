919123e6473b4c5ade2af8bf5973424df2fcf1b8
Pavan raju <pavankumarraju7@gmail.com>
2022-08-16 18:47:33 +0530

    string and in buit string

---
+++ jsstring.js
@@ -0,0 +1,17 @@
+var x = 100;
+var y = 200;
+   
+var z1 = x+y;
+var z2 = x.toString() + y;
+   
+console.log(z1); // Output: 300 
+console.log(z2); // Output: 100200
+
+var x = "some ";
+var y = "string";
+   
+console.log(x.concat(y)); // Output: some string
+
+var word = "Straße";
+
+console.log(word.toUpperCase()) // STRASSE
